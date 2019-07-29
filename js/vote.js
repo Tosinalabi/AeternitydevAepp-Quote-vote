2d5e27ec7daf89b57b05471ec4dd1e6244ec2e8a
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2019-07-29 03:23:25 +0100

    @emmanuelJet suggested update

--- js/vote.js
+++ js/vote.js
@@ -41,17 +41,40 @@ function renderQuotes() {
   $('#quoteBody').html(rendered);
 }
 
+async function callStatic(func, args) {
+  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  console.log('Functions: ', func);
+  console.log('Arguments: ', args);
+  const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
+  console.log('calledGet: ', calledGet);
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+  return decodedGet;
+}
+
+async function contractCall(func, args, value) {
+  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const calledSet = await contract.call(func, args, {amount: value}).catch(e => console.error(e));
+  return calledSet;
+}
+
 window.addEventListener('load', async () => {
   $("#loader").show();
 
   client = await Ae.Aepp();
 
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
-  const calledGet = await contract.call('getQuotesLength', [], {callStatic: true}).catch(e => console.error(e));
-  console.log('calledGet', calledGet);
+  quotesLength = await callStatic('get_quotes_length', []);
 
-  const decodedGet = await calledGet.decode().catch(e => console.error(e));
-  console.log('decodedGet', decodedGet);
+  for (let i = 1; i <= quotesLength; i++) {
+
+    const quote = await callStatic('get_quote', [i]);
+
+    quoteArray.push({
+      creatorName: quote.name,
+      quoteUrl: quote.url,
+      index: i,
+      votes: quote.quoteCount,
+    })
+  }
 
   renderQuotes();
 
@@ -59,16 +82,25 @@ window.addEventListener('load', async () => {
 });
 
 jQuery("#quoteBody").on("click", ".voteBtn", async function(event){
+  $("#loader").show();
   const value = $(this).siblings('input').val();
   const dataIndex = event.target.id;
+
+  await contractCall('vote_quote', [dataIndex], value);
+
   const foundIndex = quoteArray.findIndex(quote => quote.index == dataIndex);
+  console.log(foundIndex);
   quoteArray[foundIndex].votes += parseInt(value, 10);
   renderQuotes();
+  $("#loader").hide();
 });
 
 $('#registerBtn').click(async function(){
-  var name = ($('#regName').val()),
+  $("#loader").show();
+  let name = ($('#regName').val()),
       url = ($('#regUrl').val());
+
+  await contractCall('register_quote', [url, name], 0);
 
   quoteArray.push({
     creatorName: name,
@@ -78,4 +110,5 @@ $('#registerBtn').click(async function(){
   })
 
   renderQuotes();
+  $("#loader").hide();
 });
